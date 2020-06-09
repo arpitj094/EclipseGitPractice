d433a2ad98c1d09934d983410493888dbfb887ee
arpitj094 <arpitj094@gmail.com>
2020-06-09 13:12:02 +0530

    Modified from eclispe

--- GitPracticeDemoProj1/src/com/nt/test/Test.java
+++ GitPracticeDemoProj1/src/com/nt/test/Test.java
@@ -5,6 +5,6 @@ import com.nt.beans.Utility;
 public class Test {
 	public static void main(String[] args) {
 		Utility u = new Utility();		
-		System.out.println("Today's Date "+ u.getSysDate());
+		System.out.println("Today's Date "+ u.getSysDate()+"Modified from eclipse");
 	}
 }
