0b22c2270faa43a559789af0d5172d7a81e9f69d
arpitj094 <arpitj094@gmail.com>
2020-06-10 13:58:05 +0530

    Utility.java modified

--- GitPracticeDemoProj1/src/com/nt/beans/Utility.java
+++ GitPracticeDemoProj1/src/com/nt/beans/Utility.java
@@ -3,12 +3,13 @@ package com.nt.beans;
 import java.util.Date;
 
 public class Utility {
-	
+
 	public Date getSysDate() {
 		return new Date();
 	}
-	
-	public String printMessage(){
-	  System.out.println("Hello");	
+
+	public String printMessage() {
+		System.out.println("Hello");
+		return "Hello";
 	}
 }
